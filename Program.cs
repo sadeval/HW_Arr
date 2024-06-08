7017f292fff64292f1abaa051be68586505fc4ec
sadeval <162736837+sadeval@users.noreply.github.com>
2024-06-08 13:59:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NumberToRoman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            string romanNumber = LetterNumber(value);
+            Console.WriteLine($"Римское число: {romanNumber}");
+        }
+
+        static string LetterNumber(int num)
+        {
+            int[] numbers = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string[] letters = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+            string digitRoman = ""; // Римское число.
+
+            // конвертируем
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                while (num >= numbers[i])
+                {
+                    digitRoman += letters[i];
+                    num -= numbers[i];
+                }
+            }
+            return digitRoman;
+        }
+    }
+}
